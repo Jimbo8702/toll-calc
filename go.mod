44e6152ff14be4e2ec6f92774d49a6f34675f45b
James Sgarella <Jimbo8702@gmail.com>
2023-08-23 11:04:01 -0400

    obu for test data and first service data_receiver

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/Jimbo8702/toll-calc
+
+go 1.20
+
+require github.com/gorilla/websocket v1.5.0 // indirect
---
+++ obu/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"math"
+	"math/rand"
+	"time"
+
+	"github.com/Jimbo8702/toll-calc/types"
+	"github.com/gorilla/websocket"
+)
+
+const wsEndpoint = "ws://127.0.0.1:3000/ws"
+
+var sendInterval = time.Second 
+
+func genCoord() (float64) {
+	n := float64(rand.Intn(100) + 1)
+	f := rand.Float64()
+	return n + f
+}
+
+func genLatLong() (float64, float64) {
+	return genCoord(), genCoord()
+}
+
+func generateOBUIDS(n int) []int {
+	ids := make([]int, n)
+	for i := 0; i < n; i++ {
+		ids[i] = rand.Intn(math.MaxInt)
+	}
+	return ids
+}
+
+func main() {
+	obuIDS := generateOBUIDS(100)
+	conn, _, err := websocket.DefaultDialer.Dial(wsEndpoint, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for {
+		for i := 0; i < len(obuIDS); i++ {
+			lat, long := genLatLong()
+			data := types.OBUData{
+				OBUID: obuIDS[i],
+				Lat: lat,
+				Long: long,
+			}
+			if err := conn.WriteJSON(data); err != nil {
+				log.Fatal(err)
+			}
+			fmt.Printf("%+v\n", data)
+		}
+		time.Sleep(sendInterval)
+	}
+}
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
